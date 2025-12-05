041c7f06e17f7e809351098d712d422cc355b001
Brad Edwards <j.bradley.edwards@gmail.com>
2025-12-04 21:04:11 -0800

    Removed all unneeded APTL code

--- containers/gaming-api/api/src/database.rs
+++
@@ -1,7 +0,0 @@
-use sqlx::SqlitePool;
-
-const DATABASE_URL: &str = "sqlite:./data/gaming.db";
-
-pub async fn get_database_pool() -> Result<SqlitePool, sqlx::Error> {
-    SqlitePool::connect(DATABASE_URL).await
-}
--- containers/minecraft-server/rebuild_container.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker compose stop minecraft-server
-docker compose rm -f minecraft-server
-docker compose up --build -d minecraft-server

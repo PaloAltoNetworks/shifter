40447b5e5fcacfc91f944974998f731f8a89d1f3
Brad Edwards <j.bradley.edwards@gmail.com>
2025-12-05 16:34:08 -0800

    Add OIDC auth for Github and AWS
    Add base infra, single env only for demo
    
    add base infra ci/cd
    
    Add utility for bumping versions. Current strategy is lockstep.

---
+++ terraform/modules/ecr/main.tf
@@ -0,0 +1,42 @@
+resource "aws_ecr_repository" "this" {
+  name                 = var.repository_name
+  image_tag_mutability = var.image_tag_mutability
+
+  image_scanning_configuration {
+    scan_on_push = var.scan_on_push
+  }
+
+  tags = var.tags
+}
+
+resource "aws_ecr_lifecycle_policy" "this" {
+  count      = var.lifecycle_policy != null ? 1 : 0
+  repository = aws_ecr_repository.this.name
+  policy     = var.lifecycle_policy
+}
+
+# Default lifecycle policy to keep last 30 images
+locals {
+  default_lifecycle_policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep last 30 images"
+        selection = {
+          tagStatus     = "any"
+          countType     = "imageCountMoreThan"
+          countNumber   = 30
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_ecr_lifecycle_policy" "default" {
+  count      = var.lifecycle_policy == null ? 1 : 0
+  repository = aws_ecr_repository.this.name
+  policy     = local.default_lifecycle_policy
+}

279eee5de8ff94ad5dfc079df2466eb73fbcdf29
Brad Edwards <j.bradley.edwards@gmail.com>
2025-12-07 14:41:12 -0800

    Remove unnecessary env user

--- terraform/global/iam/portal-users.tf
+++ terraform/global/iam/portal-users.tf
@@ -1,45 +1,8 @@
-# Portal IAM Users
+# Portal IAM User
 #
-# Dedicated users for portal container to access Secrets Manager.
-# Credentials passed as environment variables - works in local dev and prod.
-
-# ------------------------------------------------------------------------------
-# Dev User
-# ------------------------------------------------------------------------------
-
-resource "aws_iam_user" "portal_dev" {
-  name = "shifter-portal-dev"
-  path = "/shifter/"
-
-  tags = {
-    Project     = "shifter"
-    Environment = "dev"
-    Purpose     = "Portal container secrets access"
-  }
-}
-
-resource "aws_iam_user_policy" "portal_dev_secrets" {
-  name = "secrets-read"
-  user = aws_iam_user.portal_dev.name
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid    = "ReadDevSecrets"
-        Effect = "Allow"
-        Action = [
-          "secretsmanager:GetSecretValue"
-        ]
-        Resource = "arn:aws:secretsmanager:${var.aws_region}:${data.aws_caller_identity.current.account_id}:secret:shifter-dev-portal-*"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_access_key" "portal_dev" {
-  user = aws_iam_user.portal_dev.name
-}
+# Dedicated user for portal container to access Secrets Manager in prod.
+# Credentials passed as environment variables via GitHub Secrets.
+# Local dev uses docker-compose with local Postgres - no AWS needed.
 
 # ------------------------------------------------------------------------------
 # Prod User
@@ -82,17 +45,6 @@ resource "aws_iam_access_key" "portal_prod" {
 # ------------------------------------------------------------------------------
 # Outputs
 # ------------------------------------------------------------------------------
-
-output "portal_dev_access_key_id" {
-  description = "Access key ID for portal dev user"
-  value       = aws_iam_access_key.portal_dev.id
-}
-
-output "portal_dev_secret_access_key" {
-  description = "Secret access key for portal dev user"
-  value       = aws_iam_access_key.portal_dev.secret
-  sensitive   = true
-}
 
 output "portal_prod_access_key_id" {
   description = "Access key ID for portal prod user"

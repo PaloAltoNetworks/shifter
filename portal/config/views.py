bd70852ae7e332e9a16e75efc5b91b0523da498a
Brad Edwards <j.bradley.edwards@gmail.com>
2025-12-08 09:50:29 -0800

    Add improved landing page

--- portal/config/views.py
+++ portal/config/views.py
@@ -1,31 +1,8 @@
 """Simple views for the portal."""
 
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    """Landing page."""
-    if request.user.is_authenticated:
-        html = f"""
-        <!DOCTYPE html>
-        <html>
-        <head><title>Shifter Portal</title></head>
-        <body>
-            <h1>Shifter Portal</h1>
-            <p>Welcome, {request.user.username}</p>
-            <p><a href="/oidc/logout/">Logout</a></p>
-        </body>
-        </html>
-        """
-    else:
-        html = """
-        <!DOCTYPE html>
-        <html>
-        <head><title>Shifter Portal</title></head>
-        <body>
-            <h1>Shifter Portal</h1>
-            <p><a href="/oidc/authenticate/">Login with Cognito</a></p>
-        </body>
-        </html>
-        """
-    return HttpResponse(html)
+    """Landing page - coming soon."""
+    return render(request, "coming_soon.html")

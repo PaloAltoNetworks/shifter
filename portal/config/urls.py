7e147b7139d39bb1206b78b78c4e1a3b1ec12690
Brad Edwards <j.bradley.edwards@gmail.com>
2025-12-07 22:57:26 -0800

    Add temp landing page for target after login

--- portal/config/urls.py
+++ portal/config/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from config.views import home
+
 urlpatterns = [
+    path("", home, name="home"),
     path("admin/", admin.site.urls),
     path("health/", include("health_check.urls")),
     path("oidc/", include("mozilla_django_oidc.urls")),

602fd256ffea4c767653c09e416f4485f895f5a3
Brad Edwards <j.bradley.edwards@gmail.com>
2025-12-07 20:20:15 -0800

    Add Cognito module with pre-signup Lambda for email domain restriction

---
+++ terraform/modules/portal/cognito/variables.tf
@@ -0,0 +1,48 @@
+variable "name_prefix" {
+  description = "Prefix for resource names"
+  type        = string
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "cognito_domain_prefix" {
+  description = "Domain prefix for Cognito hosted UI (must be globally unique)"
+  type        = string
+}
+
+variable "callback_urls" {
+  description = "OAuth callback URLs"
+  type        = list(string)
+}
+
+variable "logout_urls" {
+  description = "OAuth logout URLs"
+  type        = list(string)
+}
+
+variable "allowed_email_domains" {
+  description = "List of allowed email domains for signup (e.g., paloaltonetworks.com)"
+  type        = list(string)
+  default     = []
+}
+
+variable "allowed_emails" {
+  description = "List of specific allowed emails (for external users)"
+  type        = list(string)
+  default     = []
+}
+
+variable "deletion_protection" {
+  description = "Enable deletion protection on user pool"
+  type        = bool
+  default     = true
+}
+
+variable "tags" {
+  description = "Tags to apply to resources"
+  type        = map(string)
+  default     = {}
+}

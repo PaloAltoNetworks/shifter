ee2f7c1c249dc0855f04b09f7dde0592100493e4
Brad Edwards <j.bradley.edwards@gmail.com>
2025-12-07 20:37:45 -0800

    Add range environment config

---
+++ terraform/environments/prod/range/outputs.tf
@@ -0,0 +1,21 @@
+# Range environment outputs
+
+output "vpc_id" {
+  description = "ID of the range VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "vpc_cidr" {
+  description = "CIDR block of the range VPC"
+  value       = module.vpc.vpc_cidr
+}
+
+output "internet_gateway_id" {
+  description = "ID of the internet gateway"
+  value       = module.vpc.internet_gateway_id
+}
+
+output "public_route_table_id" {
+  description = "ID of the public route table (for user subnet associations)"
+  value       = module.vpc.public_route_table_id
+}

11251acf2800a3d2591074aa47424e806cdd4c12
Brad Edwards <j.bradley.edwards@gmail.com>
2025-12-11 00:19:47 -0800

    Copy shared provisioner lambda code properly.

--- terraform/environments/prod/portal/main.tf
+++ terraform/environments/prod/portal/main.tf
@@ -9,6 +9,14 @@ terraform {
       source  = "hashicorp/random"
       version = "~> 3.0"
     }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.0"
+    }
   }
 }
 

f6a4fdaa89b4fb7cc8ec90e12925438ea8feff64
Brad Edwards <j.bradley.edwards@gmail.com>
2025-12-12 00:43:31 -0800

    Infra for bootstrapping acct

---
+++ terraform/environments/dev/variables.tf
@@ -0,0 +1,12 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-2"
+}
+
+variable "portal_repository_name" {
+  description = "Name for the portal ECR repository"
+  type        = string
+  default     = "shifter-dev-portal"
+}
+

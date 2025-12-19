e8fa121a47044a8b01442f9008c93e53b89bcaed
Brad Edwards <j.bradley.edwards@gmail.com>
2025-12-18 21:54:49 -0800

    Terraform vars and env setup

--- terraform/modules/portal/cognito/variables.tf
+++ terraform/modules/portal/cognito/variables.tf
@@ -3,6 +3,11 @@ variable "name_prefix" {
   type        = string
 }
 
+variable "environment" {
+  description = "Environment name (e.g., prod, dev) - used for logging"
+  type        = string
+}
+
 variable "aws_region" {
   description = "AWS region"
   type        = string

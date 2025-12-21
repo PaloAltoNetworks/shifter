e5ca5bf4461f77a289095dd6d2add224c7aafbc7
Brad Edwards <j.bradley.edwards@gmail.com>
2025-12-21 09:09:47 -0800

    Fix key administration permissions

--- terraform/modules/pulumi-state/main.tf
+++ terraform/modules/pulumi-state/main.tf
@@ -103,17 +103,41 @@ resource "aws_kms_key" "pulumi_secrets" {
   deletion_window_in_days = 7
   enable_key_rotation     = true
 
-  # Key policy: allow account root + provisioner role
+  # Key policy: allow account root + current caller for administration
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Sid    = "AllowAccountRoot"
+        Sid    = "EnableRootAccountAccess"
         Effect = "Allow"
         Principal = {
           AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
         }
         Action   = "kms:*"
+        Resource = "*"
+      },
+      {
+        Sid    = "AllowKeyAdministration"
+        Effect = "Allow"
+        Principal = {
+          AWS = data.aws_caller_identity.current.arn
+        }
+        Action = [
+          "kms:Create*",
+          "kms:Describe*",
+          "kms:Enable*",
+          "kms:List*",
+          "kms:Put*",
+          "kms:Update*",
+          "kms:Revoke*",
+          "kms:Disable*",
+          "kms:Get*",
+          "kms:Delete*",
+          "kms:ScheduleKeyDeletion",
+          "kms:CancelKeyDeletion",
+          "kms:TagResource",
+          "kms:UntagResource"
+        ]
         Resource = "*"
       }
     ]

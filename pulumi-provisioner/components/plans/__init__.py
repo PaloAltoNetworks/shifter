a0581f00ec1d464ad92b9a819b9c767b8cddf9f0
Brad Edwards <j.bradley.edwards@gmail.com>
2025-12-25 10:44:19 -0800

    Extract DC setup to BootstrapPlan to make setup testable

--- pulumi-provisioner/components/plans/__init__.py
+++ pulumi-provisioner/components/plans/__init__.py
@@ -1,5 +1,6 @@
 """Setup plans for different instance types."""
 
+from .bootstrap import BootstrapPlan
 from .dc_setup import DCSetupPlan
 
-__all__ = ["DCSetupPlan"]
+__all__ = ["BootstrapPlan", "DCSetupPlan"]

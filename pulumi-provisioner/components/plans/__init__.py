d65f21c46024773684bff4d1193314c394a341f8
Brad Edwards <j.bradley.edwards@gmail.com>
2025-12-27 21:18:21 -0800

    Migrate all instances to Shifter Engine pattern

--- pulumi-provisioner/components/plans/__init__.py
+++ pulumi-provisioner/components/plans/__init__.py
@@ -2,6 +2,18 @@
 
 from .bootstrap import BootstrapPlan
 from .dc_setup import DCSetupPlan
+from .domain_join import DomainJoinPlan
+from .kali_setup import KaliSetupPlan
+from .linux_bootstrap import LinuxBootstrapPlan
+from .linux_xdr_agent_install import LinuxXDRAgentInstallPlan
 from .xdr_agent_install import XDRAgentInstallPlan
 
-__all__ = ["BootstrapPlan", "DCSetupPlan", "XDRAgentInstallPlan"]
+__all__ = [
+    "BootstrapPlan",
+    "DCSetupPlan",
+    "DomainJoinPlan",
+    "KaliSetupPlan",
+    "LinuxBootstrapPlan",
+    "LinuxXDRAgentInstallPlan",
+    "XDRAgentInstallPlan",
+]

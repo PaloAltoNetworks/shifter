9ebf8bfefbc5c510799cf04383a297f4edcac031
Brad Edwards <j.bradley.edwards@gmail.com>
2025-12-30 10:01:03 -0800

    Allow key updates

--- terraform/modules/range/vpc/alarms.tf
+++ terraform/modules/range/vpc/alarms.tf
@@ -14,14 +14,39 @@ resource "aws_kms_key" "sns" {
 
   policy = jsonencode({
     Version = "2012-10-17"
+    Id      = "key-policy-1"
     Statement = [
       {
-        Sid    = "AllowRootAccount"
+        Sid    = "EnableIAMUserPermissions"
         Effect = "Allow"
         Principal = {
           AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
         }
         Action   = "kms:*"
+        Resource = "*"
+      },
+      {
+        Sid    = "AllowKeyAdministration"
+        Effect = "Allow"
+        Principal = {
+          AWS = data.aws_caller_identity.current.arn
+        }
+        Action = [
+          "kms:Create*",
+          "kms:Describe*",
+          "kms:Enable*",
+          "kms:List*",
+          "kms:Put*",
+          "kms:Update*",
+          "kms:Revoke*",
+          "kms:Disable*",
+          "kms:Get*",
+          "kms:Delete*",
+          "kms:TagResource",
+          "kms:UntagResource",
+          "kms:ScheduleKeyDeletion",
+          "kms:CancelKeyDeletion"
+        ]
         Resource = "*"
       },
       {

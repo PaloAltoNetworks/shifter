e61fcdeee7702fbfd361151a6c6b505cd8491d31
Brad Edwards <j.bradley.edwards@gmail.com>
2025-12-31 02:07:25 -0800

    NGFW UX flows

--- portal/mission_control/routing.py
+++ portal/mission_control/routing.py
@@ -2,8 +2,9 @@
 
 from django.urls import re_path
 
-from mission_control.consumers import SSHConsumer
+from mission_control.consumers import NGFWProvisioningConsumer, SSHConsumer
 
 websocket_urlpatterns = [
     re_path(r"ws/terminal/(?P<range_id>\d+)/(?P<instance>kali|victim)/$", SSHConsumer.as_asgi()),
+    re_path(r"ws/ngfw/(?P<ngfw_id>\d+)/status/$", NGFWProvisioningConsumer.as_asgi()),
 ]

6a67f638ae174924d46822c131578ab4dffdbfe5
Brad Edwards <j.bradley.edwards@gmail.com>
2025-12-31 21:07:26 -0800

    Add failing tests for service stubs

---
+++ shifter/shifter_platform/tests/management/test_services.py
@@ -0,0 +1,10 @@
+"""Management service interface tests."""
+
+import pytest
+
+from management import services
+
+
+class TestAuditLogging:
+    def test_log_activity(self):
+        pytest.fail("Not implemented")

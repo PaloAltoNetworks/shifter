e15b301d1743376576c0220587e397ec5aaabbc5
Brad Edwards <j.bradley.edwards@gmail.com>
2026-01-05 01:52:58 -0800

    Update backend config for prod

--- platform/terraform/environments/prod/backend.tf
+++ platform/terraform/environments/prod/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "shifter-infra-514fdad5-dc1c-4aa4-acdc-ab44ff814936"
+    bucket         = "shifter-infra-6b0a7ffb-5a68-471c-8280-c4882ce371d0"
     key            = "shifter/prod/terraform.tfstate"
     region         = "us-east-2"
-    dynamodb_table = "shifter-terraform-514fdad5-dc1c-4aa4-acdc-ab44ff814936"
+    dynamodb_table = "shifter-terraform-6b0a7ffb-5a68-471c-8280-c4882ce371d0"
     encrypt        = true
   }
 }

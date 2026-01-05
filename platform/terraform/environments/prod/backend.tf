7240af829ad8b8626de12082ad778eb2707a1f8a
Brad Edwards <j.bradley.edwards@gmail.com>
2026-01-05 01:57:24 -0800

    [skip quality] extend asg grace period
    prod terraform artifacts and deploy script from testing

--- platform/terraform/environments/prod/backend.tf
+++ platform/terraform/environments/prod/backend.tf
@@ -6,4 +6,25 @@ terraform {
     dynamodb_table = "shifter-terraform-6b0a7ffb-5a68-471c-8280-c4882ce371d0"
     encrypt        = true
   }
+
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = {
+      Environment = "prod"
+      Project     = "shifter"
+      ManagedBy   = "terraform"
+    }
+  }
 }

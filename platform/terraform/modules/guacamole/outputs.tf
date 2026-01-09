938270d8a1ae8bc0df479b473f7d20382c0e822b
Brad Edwards <j.bradley.edwards@gmail.com>
2026-01-09 09:59:50 -0800

    Clean up alb/acm outputs

--- platform/terraform/modules/guacamole/outputs.tf
+++ platform/terraform/modules/guacamole/outputs.tf
@@ -23,23 +23,8 @@ output "guacamole_client_service_name" {
 }
 
 # ------------------------------------------------------------------------------
-# ALB Outputs
+# Target Group Output
 # ------------------------------------------------------------------------------
-
-output "alb_arn" {
-  description = "ARN of the Guacamole ALB"
-  value       = aws_lb.guacamole.arn
-}
-
-output "alb_dns_name" {
-  description = "DNS name of the Guacamole ALB"
-  value       = aws_lb.guacamole.dns_name
-}
-
-output "alb_zone_id" {
-  description = "Zone ID of the Guacamole ALB (for Route53 alias)"
-  value       = aws_lb.guacamole.zone_id
-}
 
 output "target_group_arn" {
   description = "ARN of the Guacamole target group"
@@ -47,27 +32,8 @@ output "target_group_arn" {
 }
 
 # ------------------------------------------------------------------------------
-# ACM Certificate Outputs
-# ------------------------------------------------------------------------------
-
-output "certificate_arn" {
-  description = "ARN of the ACM certificate"
-  value       = aws_acm_certificate.guacamole.arn
-}
-
-output "certificate_domain_validation_options" {
-  description = "Domain validation options for DNS validation"
-  value       = aws_acm_certificate.guacamole.domain_validation_options
-}
-
-# ------------------------------------------------------------------------------
 # Security Group Outputs
 # ------------------------------------------------------------------------------
-
-output "alb_security_group_id" {
-  description = "ID of the ALB security group"
-  value       = aws_security_group.alb.id
-}
 
 output "guacamole_client_security_group_id" {
   description = "ID of the guacamole-client security group"
